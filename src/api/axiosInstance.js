9927979b9f16255dad543307ef1021de9b916c24
Mirza Hammad Baig <mirzahammadbaig56@gmail.com>
2026-09-11 11:28:45 +0500

    feat: update axios instance base URL to production environment

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://backend-project-production-ecdd.up.railway.app/api/v1",
   withCredentials: true,
 });
 
